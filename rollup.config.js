2f8a2cf45a2ca19bc87e73e010a96a15cfa4bab1
Vahe <vahegian@gmail.com>
2023-01-02 22:24:27 +0100

    update: theme colors

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
-import resolve from "@rollup/plugin-node-resolve";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import del from 'rollup-plugin-delete';
+import terser from '@rollup/plugin-terser'
 
 const packageJson = require("./package.json");
 
@@ -12,16 +13,6 @@ export default [
   {
     input: "src/index.ts",
     output: [
-      // {
-      //   file: packageJson.main,
-      //   format: "cjs",
-      //   sourcemap: true,
-      // },
-      // {
-      //   file: packageJson.module,
-      //   format: "esm",
-      //   sourcemap: true,
-      // },
       {
         file: packageJson.main,
         format: "esm",
@@ -31,14 +22,15 @@ export default [
     plugins: [
       del({ targets: 'dist/*' }),
       peerDepsExternal(),
-      resolve(),
+      nodeResolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
       postcss({
         minimize: true,
         modules: false,
         extract: "styles.min.css"
       }),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      terser()
     ],
   },
   {
--- src/components/ThemeSwitch/ThemeSwitch.stories.tsx
+++ src/components/ThemeSwitch/ThemeSwitch.stories.tsx
@@ -9,18 +9,29 @@ export default {
   component: ThemeSwitch,
 } as ComponentMeta<typeof ThemeSwitch>;
 
-const Template: ComponentStory<typeof ThemeSwitch> = (args) => <ThemeSwitch {...args} />;
+const Template: ComponentStory<typeof ThemeSwitch> = (args) => {
+  return <>
+    <ThemeSwitch {...args} />
+    <div style={{width: '100%', 
+                justifyContent: 'center', 
+                alignItems: 'center',
+                display: 'flex',
+                flexDirection: 'column'}}>
+    </div>
+  </>
+}
 const Template2: ComponentStory<typeof ThemeSwitch> = (args) => {
-  return <div style={{display:"flex", 
-                      justifyContent:"space-between", 
-                      alignItems: "center", 
-                      backgroundColor: "var(--as-light)",
-                      padding: "1rem 0.5rem"}}>
+  return <div style={{
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "1rem 0.5rem"
+  }}>
     <ThemeSwitch {...args} />
     <button className='as-btn'>Default</button>
     <button className='as-btn as-bg-warning'>Warning</button>
     <button className='as-btn as-btn-primary'>Primary</button>
-    <button className='as-btn' style={{backgroundColor:'var(--as-primary-dim)'}}>Primary Dim</button>
+    <button className='as-btn' style={{ backgroundColor: 'var(--as-primary-dim)' }}>Primary Dim</button>
     <button className='as-btn as-btn-secondary'>Secondary</button>
     <button className='as-btn as-bg-danger'>Danger</button>
     <button className='as-btn as-bg-success'>Success</button>
@@ -28,15 +39,19 @@ const Template2: ComponentStory<typeof ThemeSwitch> = (args) => {
 }
 
 const Template3: ComponentStory<typeof ThemeSwitch> = (args) => {
-  return <div style={{display:"flex", 
-                      justifyContent:"space-between", 
-                      alignItems: "center", 
-                      backgroundColor: "var(--as-light)",
-                      padding: "1rem 0.5rem"}}>
+  return <div style={{
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexWrap: "wrap",
+    padding: "1rem 0.5rem"
+  }}>
     <ThemeSwitch {...args} />
-    <span className='as-text-dark as-text-bold as-text-size-xs'>Default</span>
+    <span className='as-text as-text-bold as-text-size-xs'>Default</span>
+    <span className='as-text-dark as-text-size-xs'>Dark</span>
     <span className='as-text-warning as-text-size-xs'>Warning</span>
     <span className='as-text-primary as-text-size-sm'>Primary</span>
+    <span className='as-text-anspar as-text-bold as-text-size-n'>Anspar</span>
     <span className='as-text-secondary as-text-size-n'>Secondary</span>
     <span className='as-text-danger as-text-size-l'>Danger</span>
     <span className='as-text-success as-text-size-xl'>Success</span>
@@ -44,11 +59,12 @@ const Template3: ComponentStory<typeof ThemeSwitch> = (args) => {
 }
 
 const Template4: ComponentStory<typeof ThemeSwitch> = (args) => {
-  return <div style={{display:"flex", 
-                      justifyContent:"space-between", 
-                      alignItems: "center", 
-                      backgroundColor: "var(--as-light)",
-                      padding: "1rem 0.5rem"}}>
+  return <div style={{
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "1rem 0.5rem"
+  }}>
     <ThemeSwitch {...args} />
     <span className='as-text-primary as-text-bold as-text-size-xs as-pointer as-blink'>Blink</span>
     <span className='as-text-dark as-text-size-sm as-pointer as-highlight-hover-bg'>Highlight bg</span>
@@ -62,7 +78,7 @@ const Template4: ComponentStory<typeof ThemeSwitch> = (args) => {
 export const Default = Template.bind({});
 Default.args = {
   style: {
-    width: "140px" 
+    width: "140px"
   }
 }
 
